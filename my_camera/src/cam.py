2d44b158d3da3742632e287e91cf18c35be37552
Blacklotus89898 <song.yang.chen89898@gmail.com>
2024-08-15 15:22:33 -0400

    camera id arg

--- my_camera/src/cam.py
+++ my_camera/src/cam.py
@@ -4,19 +4,26 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+import argparse
 
 class VideoCamNode:
     def __init__(self):
-        rospy.init_node('cam.py', anonymous=True)
+        #argument parser
+        parser = argparse.ArgumentParser(description='ROS Node with command-line arguments.')
+        parser.add_argument('--id', type=int, default=0, help='ID to use for this node')
+        args = parser.parse_args()
+        #id = rospy.get_param('id', 0) //param can only be acquired after init_node
+        id = args.id 
+        rospy.init_node('cam.py'+ str(id), anonymous=False)
 
         # Publisher to publish video frames
-        self.image_pub = rospy.Publisher('/cam.py', Image, queue_size=10)
+        self.image_pub = rospy.Publisher('/cam.py'+str(id), Image, queue_size=10)
 
         # Initialize the CvBridge
         self.bridge = CvBridge()
 
         # Open the video capture
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(id)
 
         # Check if the video capture opened successfully
         if not self.cap.isOpened():
